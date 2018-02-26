e30de8dc17975c81875c639cd9d32f10bda9986f
Julie K <julieskim160@gmail.com>
2018-02-25 21:59:05 -0800

    edit

--- shopping-walkthrough.js
+++ shopping-walkthrough.js
@@ -11,17 +11,6 @@ const STORE = {
 };
 
 
-
-// function searchTerm() {
-// able to search through STORE
-// maybe filter? 
-// add search bar in HTML? 
-// }
-
-// function editItem() {
-// edit title of item
-// }
-
 function generateItemElement(item, itemIndex, template) {
   return `
     <li class="js-item-index-element" data-item-index="${itemIndex}">
@@ -44,12 +33,39 @@ function generateShoppingItemsString(shoppingList){
   return items.join('');
 }
 
+function handleEditingListItem(event) {
+  let itemName = event.target.innerHTML; 
+  event.target.innerHTML = `<form> <input placeholder=${itemName} type="text">  </form>`; //input and form
+  //on enter, trigger event 
+  //event should be able to send it through pressing return
+  //put in itemName with an event 
+  //innerHTML will become new name
+  // $('.submit_on_enter').keydown(function(event) {
+  //   // enter has keyCode = 13, change it if you want to use another button
+  //   if (event.keyCode == 13) {
+  //     this.form.submit();
+  //     return false;
+  //   }
+  // }); like a dis 
+}
+
 function renderShoppingList(){
   console.log('renderShoppingList ran');
   const shoppingListItemsString= generateShoppingItemsString(STORE);
+  $('.js-shopping-list').html(shoppingListItemsString);
+  $('.js-shopping-list').on('click', '.js-shopping-item', function(event) {
+    handleEditingListItem(event);
+  })
+}
 
+function renderShoppingListParm(items) {
+  // render the shopping list in the DOM
+  console.log('`renderShoppingList` ran');
+  const shoppingListItemsString = generateShoppingItemsString(items);
+  // insert that HTML into the DOM
   $('.js-shopping-list').html(shoppingListItemsString);
 }
+
 
 function addItemToShoppingList(itemName) {
   console.log(`Adding "${itemName}" to shopping list`);
@@ -66,6 +82,7 @@ function handleNewItemSubmit(){
     renderShoppingList();
   });
 }
+
 
 function toggleCheckedForListItem(itemIndex){
   console.log('Toggling checked property for item at index ' + itemIndex);
@@ -101,11 +118,52 @@ function handleDeleteItemClicked(){
   });
 }
 
+function hideCheckedItems() {
+  const filteredCheckedItems = STORE.items.filter( item => !item.checked);
+  const shoppingListItemsString = generateShoppingItemsString(filteredCheckedItems);
+  $('.js-shopping-list').html(shoppingListItemsString);
+
+  console.log(filteredCheckedItems);
+}
+
+function handleHideCompletedItems () {
+  console.log('handleHideCompleted ran');
+  $('#check_selection').change(function() {
+    if( $('input[type=checkbox]').prop('checked')) {
+      console.log('check');
+      hideCheckedItems();
+    } else {
+      console.log('unchecked');
+      renderShoppingList();
+    }
+  });
+}
+
+function searchForItems(word) {
+  const searching = STORE.items.filter( str => str.name.startsWith(word));
+  const newHtmlString = generateShoppingItemsString(searching);
+  $('.js-shopping-list').html(newHtmlString);
+ 
+ }
+ 
+ function handleSearchedForItems() {
+  $('.js-shopping-list-entry').keyup(function() {
+    const searchWord = $('.js-shopping-list-entry').val();
+    if(searchWord === '') {
+      renderShoppingList();
+    } else {
+     searchForItems(searchWord);
+    }
+  });
+ }
+
 function handleShoppingList(){
   renderShoppingList();
   handleNewItemSubmit();
   handleItemCheckClicked();
   handleDeleteItemClicked(); 
+  handleHideCompletedItems();
+  handleSearchedForItems();
      
 }
 
